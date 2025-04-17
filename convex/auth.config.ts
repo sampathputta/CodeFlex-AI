76a278b685d25ae45fce3cc02b394077029d9318
sampathputta <puttasampath517@gmail.com>
2025-04-17 15:32:47 +0530

    project completed

---
+++ convex/auth.config.ts
@@ -0,0 +1,9 @@
+export default {
+  providers: [
+    {
+      domain: "https://generous-ferret-48.clerk.accounts.dev",
+      applicationID: "convex",
+      jwksUri: "https://generous-ferret-48.clerk.accounts.dev/.well-known/jwks.json",
+    },
+  ]
+};
